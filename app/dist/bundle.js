e743dc672da4859167c44b0ac8bfeaff0a46f18e
IvanSey <van.secret2015@gmail.com>
2020-03-19 16:04:04 +0300

    Separate list games component

--- app/dist/bundle.js
+++ app/dist/bundle.js
@@ -1,5 +1,5 @@
-var listApp = angular.module("listApp", [])
-    .controller("listController", function ($scope, $http) {
+var listApp = angular.module("listApp", []);
+listApp.controller("listController", function ($scope, $http) {
     $scope.db = {};
     $scope.limit = 20;
     $scope.data = {
@@ -36,40 +36,4 @@ var listApp = angular.module("listApp", [])
         }
     };
 });
-// listApp.controller("listController", ($scope, $http) => {
-//     $scope.db = {};
-//     $scope.limit = 20;
-//     $scope.data = {
-//         limit: 20,
-//     }
-//     $http({method: "GET", url: "/database.json"}).then((data) => {
-//         $scope.db = data.data;
-//     })
-//     $scope.changeLimit = (val) => {
-//         $scope.limit = parseInt(val);
-//     }
-//     $scope.sortGames = (field, type) => {
-//         if (field === "Name") {
-//             $scope.db.games = $scope.db.games.sort((a, b) => {
-//                 if (type === "+") {
-//                     if (a.Name.en > b.Name.en) {
-//                         return 1;
-//                     } 
-//                     if (a.Name.en < b.Name.en) {
-//                         return -1;
-//                     }
-//                     return 0;
-//                 } else if (type === "-") {
-//                     if (a.Name.en < b.Name.en) {
-//                         return 1;
-//                     } 
-//                     if (a.Name.en > b.Name.en) {
-//                         return -1;
-//                     }
-//                     return 0;
-//                 }
-//             })
-//         }
-//     }
-// });
 //# sourceMappingURL=bundle.js.map
