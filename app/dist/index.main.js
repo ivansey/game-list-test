1e5d6dc79e927fc43154c373cfe0f9055d139854
IvanSey <van.secret2015@gmail.com>
2020-03-19 14:30:45 +0300

    Add change limit games in page form

--- app/dist/index.main.js
+++ app/dist/index.main.js
@@ -2,8 +2,15 @@ var model = {};
 var listApp = angular.module("listApp", []);
 listApp.controller("listController", function ($scope, $http) {
     $scope.db = {};
+    $scope.limit = 0;
+    $scope.data = {
+        limit: 20,
+    };
     $http({ method: "GET", url: "/database.json" }).then(function (data) {
         $scope.db = data.data;
     });
+    $scope.changeLimit = function (val) {
+        $scope.limit = parseInt(val);
+    };
 });
 //# sourceMappingURL=index.main.js.map
--- app/src/index.main.ts
+++ app/src/index.main.ts
@@ -3,7 +3,16 @@ let model: object = {};
 let listApp = angular.module("listApp", []);
     listApp.controller("listController", ($scope, $http) => {
         $scope.db = {};
+        $scope.limit = 0;
+        $scope.data = {
+            limit: 20,
+        }
+        
         $http({method: "GET", url: "/database.json"}).then((data) => {
             $scope.db = data.data;
         })
+
+        $scope.changeLimit = (val) => {
+            $scope.limit = parseInt(val);
+        }
     });
