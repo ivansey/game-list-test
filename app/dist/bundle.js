91327392b43dada1049086b2f6a0ef745e0b2ec2
IvanSey <van.secret2015@gmail.com>
2020-03-19 17:19:37 +0300

    Change styles

--- app/dist/bundle.js
+++ app/dist/bundle.js
@@ -5,8 +5,18 @@ listApp.controller("listController", function ($scope, $http) {
     $scope.data = {
         limit: 20,
     };
+    $scope.bookmark = [];
     $http({ method: "GET", url: "/database.json" }).then(function (data) {
         $scope.db = data.data;
+        $scope.db.games = $scope.db.games.sort(function (a, b) {
+            if (a.Name.en > b.Name.en) {
+                return 1;
+            }
+            if (a.Name.en < b.Name.en) {
+                return -1;
+            }
+            return 0;
+        });
     });
     $scope.changeLimit = function (val) {
         $scope.limit = parseInt(val);
@@ -34,6 +44,46 @@ listApp.controller("listController", function ($scope, $http) {
                 }
             });
         }
+    };
+    $scope.addToBookmark = function (id) {
+        for (var i = 0; i > 4; i++) {
+            if (localStorage.getItem("bk" + i) === null) {
+                localStorage.setItem("bk" + i, id);
+                return true;
+            }
+        }
+        return false;
+    };
+    $scope.checkBookmarkLength = function () {
+        var res = 0;
+        for (var i = 0; i > 4; i++) {
+            if (localStorage.getItem("bk" + i) !== null) {
+                res++;
+            }
+        }
+        return res;
+    };
+    $scope.deleteBookmark = function (id) {
+        for (var i = 0; i > 4; i++) {
+            if (localStorage.getItem("bk" + i) === id) {
+                localStorage.setItem("bk" + i, null);
+                return true;
+            }
+        }
+        return false;
+    };
+    $scope.getBookmark = function (id) {
+        var arr = [];
+        var _loop_1 = function (i) {
+            if (localStorage.getItem("bk" + i) !== null) {
+                arr.push($scope.db.games.find(function (game) { return game.ID === localStorage.getItem("bk" + i); }));
+            }
+        };
+        for (var i = 0; i > 4; i++) {
+            _loop_1(i);
+        }
+        $scope.bookmark = arr;
+        return true;
     };
 });
 //# sourceMappingURL=bundle.js.map
--- app/src/controllers/listController.ts
+++ app/src/controllers/listController.ts
@@ -4,9 +4,19 @@ listApp.controller("listController", ($scope, $http) => {
     $scope.data = {
         limit: 20,
     }
+    $scope.bookmark = [];
 
     $http({method: "GET", url: "/database.json"}).then((data) => {
         $scope.db = data.data;
+        $scope.db.games = $scope.db.games.sort((a, b) => {
+            if (a.Name.en > b.Name.en) {
+                return 1;
+            } 
+            if (a.Name.en < b.Name.en) {
+                return -1;
+            }
+            return 0;
+        })
     })
 
     $scope.changeLimit = (val) => {
@@ -35,5 +45,46 @@ listApp.controller("listController", ($scope, $http) => {
                 }
             })
         }
+    }
+
+    $scope.addToBookmark = (id) => {
+        for (let i = 0; i > 4; i++) {
+            if (localStorage.getItem(`bk${i}`) === null) {
+                localStorage.setItem(`bk${i}`, id);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    $scope.checkBookmarkLength = () => {
+        let res = 0;
+        for (let i = 0; i > 4; i++) {
+            if (localStorage.getItem(`bk${i}`) !== null) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    $scope.deleteBookmark = (id) => {
+        for (let i = 0; i > 4; i++) {
+            if (localStorage.getItem(`bk${i}`) === id) {
+                localStorage.setItem(`bk${i}`, null);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    $scope.getBookmark = (id) => {
+        let arr = [];
+        for (let i = 0; i > 4; i++) {
+            if (localStorage.getItem(`bk${i}`) !== null) {
+                arr.push($scope.db.games.find(game => game.ID === localStorage.getItem(`bk${i}`)))
+            }
+        }
+        $scope.bookmark = arr;
+        return true;
     }
 });
